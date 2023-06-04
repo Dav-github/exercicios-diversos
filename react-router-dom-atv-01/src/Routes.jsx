3934039974938894cee4d9efec48b281bb2cfb63
Dave <d4v3.oliveira@gmail.com>
2023-06-04 16:00:19 -0300

    atividade de fixaxão react-router-dom

--- react-router-dom-atv-01/src/Routes.jsx
+++ react-router-dom-atv-01/src/Routes.jsx
@@ -1,13 +1,22 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet, Navigate } from "react-router-dom";
 import "./App.css";
 import Main from "./pages/Main";
 import SignIn from "./pages/SignIn";
+
+function ProtectRoutes({ redirectTo }) {
+  const isAuth = true;
+
+  return isAuth ? <Outlet /> : <Navigate to={redirectTo} />;
+}
 
 function MainRoutes() {
   return (
     <Routes>
       <Route path="/" element={<SignIn />} />
-      <Route path="/dashboard" element={<Main />} />
+      <Route element={<ProtectRoutes redirectTo={"/"} />}>
+        <Route path="/dashboard" element={<Main />} />
+      </Route>
+      <Route path="*" element={<h1>Error 404</h1>} />
     </Routes>
   );
 }
