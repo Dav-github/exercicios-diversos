123d016865b3875bd664b936d5abaa2c499c533b
Dave <d4v3.oliveira@gmail.com>
2023-06-04 22:34:17 -0300

    atividade de fixaxão react-router-dom

--- react-router-dom-atv-02/src/MainRoutes.jsx
+++ react-router-dom-atv-02/src/MainRoutes.jsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import SignIn from "./pages/SignIn";
 import Dashboard from "./pages/Dashboard";
-import Card2 from "./ components/Card2";
 
 function MainRoutes() {
   return (
