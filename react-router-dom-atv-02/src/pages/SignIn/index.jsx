2c6774dc2598b9bf9450cab80535c0459157f446
crow <d4v3.oliveira@gmail.com>
2023-06-06 23:48:21 -0300

    fix

--- react-router-dom-atv-02/src/pages/SignIn/index.jsx
+++ react-router-dom-atv-02/src/pages/SignIn/index.jsx
@@ -3,25 +3,25 @@ import logoCubos from "../../assets/logo-cubos.svg";
 import "./style.css";
 
 export default function SignIn() {
-  return (
-    <section className="container container-sign-in">
-      <form action="" className="form">
-        <div className="div-logo">
-          <img src={logoCubos} alt="logo da cubos" />
-        </div>
-        <div className="box-sign-in">
-          <label>
-            <input type="text" placeholder="E-mail" />
-          </label>
-          <label>
-            <input type="text" placeholder="Passworld" />
-          </label>
-          <p className="p-sign-in">Não tem cadastro? Clique aqui!</p>
-          <Link to="/dashboard">
-            <button className="btn">Login</button>
-          </Link>
-        </div>
-      </form>
-    </section>
-  );
+    return (
+        <section className="container container-sign-in">
+            <form action="" className="form">
+                <div className="div-logo">
+                    <img src={logoCubos} alt="logo da cubos" />
+                </div>
+                <div className="box-sign-in">
+                    <label>
+                        <input type="text" placeholder="E-mail" />
+                    </label>
+                    <label>
+                        <input type="text" placeholder="Passworld" />
+                    </label>
+                    <p className="p-sign-in">Não tem cadastro? Clique aqui!</p>
+                    <Link to="/dashboard">
+                        <button className="btn">Login</button>
+                    </Link>
+                </div>
+            </form>
+        </section>
+    );
 }
